61edadc62ce34dee83b999e97215c3c163419acd
maxxburn <tarkaklavio@gmail.com>
2021-08-05 11:48:03 +0200

    second

---
+++ Commands.cs
@@ -0,0 +1,9 @@
+using Discord.WebSocket;
+using Discord.Commands;
+
+public class CommandHandler
+{
+    private readonly DiscordSocketClient client;
+    private readonly CommandService commandService;
+
+}
