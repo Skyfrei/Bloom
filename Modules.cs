ddadc327dc03b5aa8e3f163845e732e8529eb0c2
maxxburn <tarkaklavio@gmail.com>
2021-08-06 12:36:19 +0200

    Bloom is functional

---
+++ Modules.cs
@@ -0,0 +1,38 @@
+using Discord.Commands;
+using System;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+
+namespace Modules{
+    [Group("sample")]
+    public class SampleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        => ReplyAsync('\u200B' + echo);
+
+        [Command("square")]
+        [Summary("Squares a number.")]
+        public async Task SquareAsync(
+                [Summary("The number to square.")]
+                int num)
+        {
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+
+        [Command("userinfo")]
+        [Summary 
+        ("Returns info about the current user, or the user paramter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(
+            [Summary("The (optional) user to get info from.")]
+            SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,33 +2,74 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using System.Threading;
+using Modules;
 
 namespace Bloom
 {
     class Program
     {
-        private DiscordSocketClient client;
+        private readonly DiscordSocketClient client;
+        private SampleModule module;
+        
+
         static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public Program()
+        {
+            client = new DiscordSocketClient();
+            module = new SampleModule();
+
+            client.Log += LogASync;
+            client.Ready += ReadyAsync;
+            client.MessageReceived += MessageReceivedAsync;
+        }
 
         public async Task MainAsync()
         {
-            client = new DiscordSocketClient();
             
-            client.Log += Log;
             string token = "ODcyNzY2OTMyMzk3NDg2MTUw.YQupiw.5NasIh63nbNasF5Kq2vwea8YFKY";
 
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
 
-            await Task.Delay(-1);
+            await Task.Delay(Timeout.Infinite);
         }
 
-        private Task Log(LogMessage message)
+        private Task LogASync(LogMessage log)
         {
-            Console.WriteLine(message.ToString());
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{client.CurrentUser} is connected!");
 
             return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            string description = "";
+            // The bot should never respond to itself.
+            if (message.Author.Id == client.CurrentUser.Id)
+                return;
+
+            if (message.Content.ToLower()== "!ping")
+                await message.Channel.SendMessageAsync("Pong");
+
+            if (message.Content.ToLower() == "!bloom")
+                await message.Channel.SendMessageAsync(".");
+
+            if (message.Content.ToLower() == "!name")
+                await message.Channel.SendMessageAsync("Are you blind kid?");
+
+            // if (message.Content == "square")
+            //     await module.SquareAsync()
         }
     }
 }
