7ac326a24cdeaf477c1cc0f3acd63cc2a0886f1a
maxxburn <tarkaklavio@gmail.com>
2021-08-09 11:55:25 +0200

    Bloom is fully functional with prefix "!"

--- Program.cs
+++ Program.cs
@@ -1,78 +1,54 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.WebSocket;
-using System.Threading;
 using Discord.Commands;
-using Modules;
-using System.Reflection;
+using _02_commands_framework.Services;
 
-namespace Bloom
+namespace _02_commands_framework
 {
     class Program
     {
-        private readonly DiscordSocketClient client;
-        private readonly CommandService commands;
-        
-
         static void Main(string[] args)
-        {
-            new Program().MainAsync().GetAwaiter().GetResult();
-        }
-
-        public Program()
-        {
-            client = new DiscordSocketClient();
-            commands = new CommandService();
-
-            client.Log += LogASync;
-            client.Ready += ReadyAsync;
-            client.MessageReceived += MessageReceivedAsync;
-            
-            
-        }
+            => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-            
-            string token = "ODcyNzY2OTMyMzk3NDg2MTUw.YQupiw.5NasIh63nbNasF5Kq2vwea8YFKY";
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
 
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
 
-            await Task.Delay(Timeout.Infinite);
-            await commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+                await client.LoginAsync(TokenType.Bot, "ODcyNzY2OTMyMzk3NDg2MTUw.YQupiw.5NasIh63nbNasF5Kq2vwea8YFKY");
+                await client.StartAsync();
+
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(Timeout.Infinite);
+            }
         }
 
-        private Task LogASync(LogMessage log)
+        private Task LogAsync(LogMessage log)
         {
             Console.WriteLine(log.ToString());
-            return Task.CompletedTask;
-        }
-
-        private Task ReadyAsync()
-        {
-            Console.WriteLine($"{client.CurrentUser} is connected!");
 
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(SocketMessage message)
+        private ServiceProvider ConfigureServices()
         {
-            string description = "I am Bloom, a creature whose lifespan ";
-            // The bot should never respond to itself.
-            if (message.Author.Id == client.CurrentUser.Id)
-                return;
-
-            if (message.Content.ToLower()== "!ping")
-                await message.Channel.SendMessageAsync("Pong");
-
-            if (message.Content.ToLower() == "!bloom")
-                await message.Channel.SendMessageAsync(".");
-
-            if (message.Content.ToLower() == "!name")
-                await message.Channel.SendMessageAsync("Are you blind kid?");
-
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton<PictureService>()
+                .BuildServiceProvider();
         }
     }
 }
---
+++ PublicModule.cs
@@ -0,0 +1,57 @@
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using _02_commands_framework.Services;
+
+namespace _02_commands_framework.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        public PictureService PictureService { get; set; }
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+
+        [Command("cat")]
+        public async Task CatAsync()
+        {
+            var stream = await PictureService.GetCatPictureAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "cat.png");
+        }
+
+        [Command("userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user = user ?? Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+        [Command("ban")]
+        [RequireContext(ContextType.Guild)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task BanUserAsync(IGuildUser user, [Remainder] string reason = null)
+        {
+            await user.Guild.AddBanAsync(user, reason: reason);
+            await ReplyAsync("ok!");
+        }
+
+        [Command("echo")]
+        public Task EchoAsync([Remainder] string text)
+            => ReplyAsync('\u200B' + text);
+
+        [Command("list")]
+        public Task ListAsync(params string[] objects)
+            => ReplyAsync("You listed: " + string.Join("; ", objects));
+
+        [Command("guild_only")]
+        [RequireContext(ContextType.Guild, ErrorMessage = "Sorry, this command must be ran from within a server, not a DM!")]
+        public Task GuildOnlyCommand()
+            => ReplyAsync("Nothing to see here!");
+    }
+}
